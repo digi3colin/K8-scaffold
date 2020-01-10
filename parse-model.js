ebb9b6bac033b3724c30d6120fa9d1232b87f121
Colin Leung <colin@digi3studio.com>
2020-01-10 09:48:57 +0800

    update for ORM accept options as argument.

--- parse-model.js
+++ parse-model.js
@@ -17,8 +17,8 @@ function parseClass(model){
 const ORM = K8.require('ORM');
 
 class ${model.className} extends ORM{
-  constructor(id, db) {
-    super(id, db);
+  constructor(id, options) {
+    super(id, options);
     if(id)return;
 
     //foreignKeys
